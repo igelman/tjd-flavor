22f18c5d02578e0eef67b899e8a9a2c01100b404
Alan Igelman <alan@igelman.com>
2014-01-16 20:55:38 -0500

    Add setUp to unit tests.

--- functions/tjd-extensions/tests/test-tmt-get-custom-fields.php
+++ functions/tjd-extensions/tests/test-tmt-get-custom-fields.php
@@ -10,6 +10,40 @@ require_once '../classes/tmt-get-custom-fields-class.php';
 
 
 class TestGetCustomFields extends PHPUnit_Framework_TestCase {
+
+	public function setUp() {
+		// Create post object
+		$this->postFields = array(
+			'post_title'    => 'My post',
+			'post_content'  => 'This is my post.',
+			'post_status'   => 'publish',
+			'post_author'   => 1,
+		);
+		
+		$this->postCustomFields = array(
+			'url'				=> "",
+			'logo_image'		=> "",
+			'post_expiration'	=> "",
+			'deal_expiration'	=> "",
+			'coupon_codes'		=> "",
+		);
+		
+
+		// Insert the post into the database
+		$this->postId = wp_insert_post( $this->postFields );
+		foreach ($this->postCustomFields as $key => $value) {
+			update_post_meta($this->postId, $key, $value);
+		}
+		
+		
+		echo "postId: " . $this->postId . PHP_EOL;
+	}
+	
+	public function testTest() {
+		get_post_meta( $this->postId, $key, FALSE );
+		$this->assertTrue(TRUE);
+	}
+
 }
 
 ?>
